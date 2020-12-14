19a86be638bf3361c406c3ec8a790ac789391c94
IgorInterliche <interliches@gmail.com>
2020-12-14 14:29:44 -0300

    exercício 73 adicionad0

---
+++ ex_73_tuplas_futebol.py
@@ -0,0 +1,14 @@
+times = ('Corintians', 'Palmeiras', 'Santos', 'Grêmio',
+         'Cruzeiro', 'Vasco', 'Botafogo', 'Atlético-PR',
+         'Bahia', 'São Paulo', 'Fluminense', 'Chapecoense',
+         'Ponte Preta',)
+print('-=' * 20)
+print(f'Lista de times do Brasileirão: {times}')
+print('-=' * 20)
+print(f'Os 5 primeiros são {times[0:5]}')
+print('-=' * 20)
+print(f'Os 4 últimos são {times[-4]}')
+print('-=' * 20)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-=' * 20)
+print(f'O Chapecoense está na {times.index("Chapecoense")+1}º posição')
