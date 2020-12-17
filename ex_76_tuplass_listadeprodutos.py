b9c1a964de1db2449b17d18972c12fa4717dd626
IgorInterliche <interliches@gmail.com>
2020-12-17 17:35:43 -0300

    loja eletronica aeiou

---
+++ ex_76_tuplass_listadeprodutos.py
@@ -0,0 +1,18 @@
+listagem = ('Monitor', 989.99,
+          'Teclado', 50.99,
+          'Mouse', 40.99,
+          'Headset', 80.00,
+          'Controle sem fio', 399.99,
+          'Caixas de som', 99.99,
+        'Carregador para celular', 29.99)
+
+print('-' * 40)
+print(f'{"Listagem de Preços":^40}')
+print('-' * 40)
+
+
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
