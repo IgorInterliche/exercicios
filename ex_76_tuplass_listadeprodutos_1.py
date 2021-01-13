8c0ecbc2f0d513104609469914197c951014527d
Mauro Interliche de Oliveira <interlich@gmail.com>
2021-01-13 08:32:11 -0300

    ajuste

--- ex_76_tuplass_listadeprodutos_1.py
+++ ex_76_tuplass_listadeprodutos_1.py
@@ -9,23 +9,22 @@ listagem = ('Monitor', 989.99, 'Eletronico',
         'Carregador para celular', 29.99, 'utilitario',
             'Testador de carga',100.00, 'utilitario')
 
+print()
+print('Listagem de Preços')
+print('-' * 55)
 
-print('-' * 40)
-print(f'{"Listagem de Preços":^40}')
-print('-' * 40)
-
-col = 0
+col = 1
 num_col = 4
 
 for pos in range(0, len(listagem)):
-    if col == 0:
+    if col == 1:
         print(f'{listagem[pos]:.<30}', end='')
-        col = 1
-    elif col == 1:
-        print(f'R${listagem[pos]:>7.2f}', end='')
         col = 2
-    elif col ==2:
+    elif col == 2:
+        print(f'R${listagem[pos]:>7.2f}', end='')
+        col = 3
+    elif col ==3:
         print(f'  {listagem[pos]}')
-        col = 0
+        col = 1
 
 
--- ex_76_tuplass_listadeprodutos_2.py
+++ ex_76_tuplass_listadeprodutos_2.py
@@ -1,30 +1,50 @@
 #Alterado para considerar qualquer numero de colunas, mas sem nenhuma formatação.
+#iniciação das variáveis-----------
+listagem = ('Monitor', 989.99,'duplo','1', 'sim',
+            'Teclado', 50.99,'simples','2', 'nao',
+            'Mouse', 40.99,'duplo','3', 'nao',
+            'Headset', 80.00,'simples','4', 'sim',
+            'Controle sem fio', 399.99,'duplo','5', 'não',
+            'Caixas de som', 99.99,'simples','6', 'sim',
+            'Carregador celular', 29.99,'duplo', '7', 'nao')
 
-# em vez de duas...
-listagem = ('Monitor', 989.99,'duplo','1', 'sim', 1,
-            'Teclado', 50.99,'simples','2', 'nao', 2,
-          'Mouse', 40.99,'duplo','3', 'nao', 3,
-          'Headset', 80.00,'simples','4', 'sim', 4,
-          'Controle sem fio', 399.99,'duplo','5', 'não',5,
-          'Caixas de som', 99.99,'simples','6', 'sim', 6,
-        'Carregador celular', 29.99,'duplo', '7', 'nao',7 )
-
-print('-' * 97)
-print(f'{"Listagem de Preços":^40}')
-print('-' * 97)
-
-#acrescentado dois contadores para reposicionar a coluna.
+des_col = ('  Descrição            ',
+           ' Valor    ',
+           ' Tipo      ',
+           ' Item   ',
+           ' Em estoque  ')
+tam_col = (20,
+           8,
+           9,
+           8,
+           10)
 col_atu = 0  #numero da coluna atual
-col_tot = 5  #total de colunas
+col_tot = len(des_col) #total de colunas
+#fim da iniciação das variaveis
 
+#impressão do cabeçalho-----
+print()
+print('Listagem de Produtos')
+print('-' * 66)
+for pos in range(0, col_tot):
+    print(des_col[pos], end='')
+print()
+print('-' * 66)
+#fim do cabeçalho
+
+#impressão das linhas detalhe----
 for pos in range(0, len(listagem)):
 
-    if col_atu < col_tot:
-        print(f'{listagem[pos]:<19}', end='')  #19 é o numero de caracteres maximo das colunas...
-        col_atu = col_atu + 1
+    campo = str(listagem[pos])
+    n_esp = tam_col[col_atu] - len(campo)
+    campo = campo + (n_esp * ' ')
 
+    if col_atu < col_tot-1:
+
+        print(' ', campo, end='')
+        col_atu = col_atu + 1
     else:
-        print(f' {listagem[pos]}')
+        print(campo)
         col_atu = 0
 
 
