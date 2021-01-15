38d915d49c251e3f5cdad69ccbda8fa4913dc284
Mauro Interliche de Oliveira <interlich@gmail.com>
2021-01-15 18:34:53 -0300

    acrescentado um comentário.

--- ex_76_tuplass_listadeprodutos_3.py
+++ ex_76_tuplass_listadeprodutos_3.py
@@ -1,14 +1,18 @@
-#Acrescentei funções para otimizar esse programa, deixando ele mais prático.
-def imp_vetor( vDes= (), vCol = ()):
-    #imprime vetor em linhas...
-    tot_col = len(vCol)
-    tot_des = len(vDes)
-    col_atu = 0
-    for pos in range(0, tot_des):
-        campo = str(vDes[pos])   #precisa converter por causa dos campos numéricos...
-        tot_esp = vCol[col_atu] - len(campo)  #tamanho do campo desejado - tamanho do campo atual
-        campo = campo + (tot_esp * ' ')
-        if col_atu < tot_col - 1:
+# Acrescentei funções para otimizar esse programa, deixando ele mais prático
+def imp_vetor(vdes, vcol):
+    # imprime vetor em linhas...
+    tot_col = len(vcol)    # Total de colunas
+    tot_itens = len(vdes)  # total de itens a serem impressos em todas as colunas#
+    col_atu = 0            # inicializa para a coluna 1 para entrar no loop do processamento...
+    for pos in range(0, tot_itens):
+        if type(vdes[pos]) == float or type(vdes[pos]) == int:
+            campo = str(vdes[pos])  # precisa converter por causa dos campos numéricos...
+        else:
+            campo = vdes[pos]
+
+        tot_esp = vcol[col_atu] - len(campo)  # Total espaços=tamanho do campo desejado - tamanho do campo atual
+        campo = campo + (tot_esp * ' ')       # acrescenta os espaços ao final do campo para ficarem todos alinhados.
+        if col_atu < tot_col - 1:             # inibe a impressão do proximo print na proxima linha.
             print(campo, end='')
             col_atu = col_atu + 1
         else:
@@ -16,22 +20,30 @@ def imp_vetor( vDes= (), vCol = ()):
             col_atu = 0
     print(' ')
 
-def soma_lista(vLista):
-    #soma os itens de uma lista (vetor)
+
+def soma_lista(vlista):  # soma os itens de uma lista (vetor)
     car_lin = 0
-    col_tot = len(vLista)
+    col_tot = len(vlista)
     for pos in range(0, col_tot):
-        car_lin = car_lin + vLista[pos]
+        if type(vlista[pos]) == float or type(vlista[pos]) == int:
+            car_lin = car_lin + vlista[pos]
+        else:
+            print('campo não númerico encontrado no vetor')
+            return 0
     return car_lin
 
-#iniciação das variáveis-----------
-listagem = ('Monitor', 989.99,'duplo','1', 'sim', 23, 'jose',
-            'Teclado', 50.99,'simples','2', 'nao', 26, 'pedro',
-            'Mouse', 40.99,'duplo','3', 'nao', 30, 'maria',
-            'Headset', 80.00,'simples','4', 'sim', 10, 'janaina',
-            'Controle sem fio', 399.99,'duplo','5', 'não', 10, 'maria eduarda',
-            'Caixas de som', 99.99,'simples','6', 'sim', 40, 'josefina',
-            'Carregador celular', 29.99,'duplo', '7', 'nao', 50, 'eliete')
+
+# main program #
+#
+# iniciação das variáveis-----------
+#
+listagem = ('Monitor', 989.99, 'duplo', '1', 'sim', 23, 'jose',
+            'Teclado', 50.99, 'simples', '2', 'nao', 26, 'pedro',
+            'Mouse', 40.99, 'duplo', '3', 'nao', 30, 'maria',
+            'Headset', 80.00, 'simples', '4', 'sim', 10, 'janaina',
+            'Controle sem fio', 399.99, 'duplo', '5', 'não', 1, 'maria eduarda',
+            'Caixas de som', 99.99, 'simples', '6', 'sim', 40, 'josefina',
+            'Carregador celular', 29.99, 'duplo', '7', 'nao', 50, 'eliete')
 
 cabecalho = ('Descrição do Produto',
              'Valor',
@@ -42,18 +54,17 @@ cabecalho = ('Descrição do Produto',
              'Nome vendedor')
 
 coluna = (23,
-           8,
-           9,
-           6,
+          8,
+          9,
+          6,
           12,
-           5,
+          5,
           20)
 
 lin_sep = '-' * soma_lista(coluna)
 
-#---------------------@
-#
-#_____execução___
+# ---------------------@
+# _____execução___
 #
 print()
 print('Listagem de Produtos')
@@ -62,8 +73,5 @@ imp_vetor(cabecalho, coluna)
 print(lin_sep)
 imp_vetor(listagem, coluna)
 print(lin_sep)
-#
-#______fim_______
-#
-
-
+#                #
+# ______fim_______#
