17903589fecd39650f38c06a172790b95df61949
Mauro Interliche de Oliveira <interlich@gmail.com>
2021-01-19 21:52:43 -0300

    criado o programa que recebe os vetores para o processamento, listadeprodutos_relatorio, a partir de um programa chamador Listadeprodutos_5a e listadeprodutos_5b, que respectivamente passam cada um dados para a produção do relatorio...

---
+++ ex_76_tuplass_listadeprodutos_1.py
@@ -0,0 +1,32 @@
+# Exercício proposta para considerar 3 colunas,
+# agora passando também mais 2 vetores:
+# 1) descrição dos cabeçalhos;
+# 2) tamanho dos campos;
+
+listagem = ('Monitor', 989.99, 'Eletronico',
+          'Teclado', 50.99, 'utilitario',
+          'Mouse', 40.99, 'utiitario',
+          'Headset', 80.00, 'eletronico',
+          'Controle sem fio', 399.99, 'utilitario',
+          'Caixas de som', 99.99, 'utilitario',
+        'Carregador para celular', 29.99, 'utilitario',
+            'Testador de carga',100.00, 'utilitario')
+
+print()
+print('Listagem de Preços')
+print('-' * 55)
+
+col = 1
+
+for pos in range(0, len(listagem)):
+    if col == 1:
+        print(f'{listagem[pos]:.<30}', end='')
+        col = 2
+    elif col == 2:
+        print(f'R${listagem[pos]:>7.2f}', end='')
+        col = 3
+    elif col ==3:
+        print(f'  {listagem[pos]}')
+        col = 1
+
+
